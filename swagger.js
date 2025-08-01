2c0104601024731c1ce3f1339d20ccee6bb40186
o-j2315 <jhernandezap23@gmail.com>
2025-07-31 22:27:06 -0600

    fix: Update Swagger configuration to use production host and remove localhost references

--- swagger.js
+++ swagger.js
@@ -7,8 +7,8 @@ const doc = {
         description: "API documentation for managing School resources",
         version: "1.0.0",
     },
-    host: "localhost:3000",
-    schemes: ["http", "https"],
+    host: "t8finalproject-schoolapi.onrender.com",
+    schemes: ["https"],
     consumes: ["application/json"],
     produces: ["application/json"],
     tags: [
